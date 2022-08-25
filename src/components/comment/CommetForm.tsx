496ee59f66d1cf8ae76d2ed7b91d0c36c321d83d
choeqq <k.chola@op.pl>
2022-08-25 18:36:30 +0200

    upadting CommentForm component

--- src/components/comment/CommetForm.tsx
+++ src/components/comment/CommetForm.tsx
@@ -1,11 +1,14 @@
 import { Box, Button, Group, Textarea } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useRouter } from "next/router";
+import { trpc } from "../../utils/trpc";
 
-function CommentForm() {
+function CommentForm({ parentId }: { parentId?: string }) {
   const router = useRouter();
 
   const permalink = router.query.permalink as string;
+
+  const { mutate, isLoading } = trpc.useMutation(["comments.add-comment"]);
 
   const form = useForm({
     initialValues: {
@@ -13,11 +16,19 @@ function CommentForm() {
     },
   });
 
-  function handleSubmit(values: { body: string }) {}
+  function handleSubmit(values: { body: string }) {
+    const payload = {
+      ...values,
+      permalink,
+      parentId,
+    };
+
+    mutate(payload);
+  }
 
   return (
     <Box mt="md" mb="md">
-      <form>
+      <form onSubmit={form.onSubmit(handleSubmit)}>
         <Textarea
           required
           placeholder="Your spicey comment"
@@ -25,7 +36,9 @@ function CommentForm() {
           {...form.getInputProps("body")}
         />
         <Group position="right" mt="md">
-          <Button type="submit">Post Comment</Button>
+          <Button loading={isLoading} type="submit">
+            {parentId ? "Post reply" : "Post Comment"}
+          </Button>
         </Group>
       </form>
     </Box>
